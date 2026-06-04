820518e01fe16c235e2e7365e09da39ab38e00e5
Youssef <youssefosamasalah51@gmail.com>
2026-06-04 15:42:30 +0300

    I changed the shape of the buttons mking them smaller and spaced apart and added them cleverly using Flask. I button and a great background to it . Now we begin creating the hardest part the function of each button

--- app.py
+++ app.py
@@ -10,18 +10,17 @@ def home ():
     center_y = 350
     rotate = 0
     circles = [
-        (3, 140),
-        (7, 340),
-        (10, 540),
-        (15, 750),
-        (20, 1000)]
+        (5, 300),
+        (10, 600),
+        (15, 1000),
+        (20, 1400)]
     for conut,radius in circles:
         rotate =+ 60
         for i in range(conut):
             angle = radians((i*360 / conut )+ rotate)
             x = center_x + radius *cos(angle)
             y = center_y + radius *sin(angle)
-            buttons.append({"x":x , "y":y})
+            buttons.append({"x":x , "y":y , "number": len(buttons) + 2 })
         
 
 
