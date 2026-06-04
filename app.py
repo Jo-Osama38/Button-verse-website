9aa9a6dbe9d753d714164b72f504f7f01b2b44e0
Youssef <youssefosamasalah51@gmail.com>
2026-06-04 03:19:02 +0300

    Iplaced the fifty buttons on the main page and tried rearranging them more than six times to improve their appearance but they still look ugly i need to find a new way to organize them

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask ,render_template
+from math import sin , cos , radians
 
 app = Flask(__name__)
 
 @app.route("/")
 def home ():
-    return render_template('index.html')
+    buttons = []
+    center_x = 600
+    center_y = 350
+    rotate = 0
+    circles = [
+        (3, 140),
+        (7, 340),
+        (10, 540),
+        (15, 750),
+        (20, 1000)]
+    for conut,radius in circles:
+        rotate =+ 60
+        for i in range(conut):
+            angle = radians((i*360 / conut )+ rotate)
+            x = center_x + radius *cos(angle)
+            y = center_y + radius *sin(angle)
+            buttons.append({"x":x , "y":y})
+        
+
+
+    return render_template('index.html' ,buttons=buttons)
 
 
 if __name__ == "__main__":
